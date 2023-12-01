1240a4dfb9b8f9b3cefd654eb162205820ae333f
Robert deLaguna <rjdelaguna@yahoo.com>
2023-12-01 12:26:40 -0800

    add seed user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,4 +6,19 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: UserSerializer.new(user)
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { success: "User Created Successfully" }
+    else
+      render json: { failure: "User Could Not Be Created" }
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email)
+  end
 end
