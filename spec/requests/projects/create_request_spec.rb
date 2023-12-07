60fc6ad833fe9a20b6fbf2386c24324cb51fced2
Nicholas McEnroe <nsmcenro@gmail.com>
2023-12-07 13:05:11 -0700

    Edit: Fulfill CI requirements for this branch

--- spec/requests/projects/create_request_spec.rb
+++ spec/requests/projects/create_request_spec.rb
@@ -43,6 +43,6 @@ describe "create a users projects" do
     message = JSON.parse(response.body, symbolize_names: true)
 
     expect(message).to be_a Hash
-    expect(message[:error]).to eq("Title can't be blank")
+    expect(message[:error][0]).to eq("Title can't be blank")
   end
 end
